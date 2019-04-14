0b6718062386914b0436d86ab302d01cd6aabb54
amiecollins <a_collins84448@fanshaweonline.ca>
2019-04-14 01:17:49 -0400

    past events page complete, just needs photos

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@
 	var button = document.querySelector("#hamburgerIcon");
 	var burgerCon = document.querySelector("#burgerCon");
 	var backToTop = document.querySelector("#bottomFloat");
+	var theButtons = document.querySelectorAll(".pastEventsTitle");
+	var galleryIcon = document.querySelectorAll(".pastEvent");
+	var thelightBox = document.querySelector(".lightBox");
+	var lightBoxImg = document.querySelector("#focusedImage");
+	var lightBoxArticle = document.querySelector("#focusedArticle");
+	var closeButton = document.querySelector("#close-button");
 
 	function hamburgerMenu() {
 		burgerCon.classList.toggle("slideToggle");
@@ -17,7 +23,29 @@
   		document.documentElement.scrollTop = 0
 	}
 
+	function toggleContent() {
+		// Toggle Past Event Content
+		console.log(this.id);
+		document.getElementById(this.id+"Content").classList.toggle("visible");	
+	}
+
+	function LightBoxOn() {
+		console.log(this.id);
+		thelightBox.classList.add("light-box-visible");
+		var eventNum = this.id.replace("Event", "");
+		lightBoxImg.classList.add(`Img${eventNum}`);
+		lightBoxArticle.innerHTML = document.getElementById(`Img${eventNum}`).innerHTML;
+	}
+
+	function LightBoxOff() {
+		console.log(this.textContent);
+		thelightBox.classList.remove("light-box-visible");
+	}
+
+
 	button.addEventListener("click", hamburgerMenu, false);
 	backToTop.addEventListener("click", scrollToTop);
-		
+	theButtons.forEach(currentButton => currentButton.addEventListener("click", toggleContent));
+	galleryIcon.forEach(icon => icon.addEventListener("click", LightBoxOn));
+	closeButton.addEventListener("click", LightBoxOff, false);
 })();
